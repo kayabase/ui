99dd348988be8c12f26c036fd80b25a0ff7f7b77
beerkaya <beerkaya@github.com>
2021-11-20 22:01:09 +0300

    vue project created

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center"><h1>KAYABASE</h1></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+  };
+</script>
+
+<style lang="scss">
+  @import "assets/css/style";
+</style>
