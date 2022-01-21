f51d116cd5ba74f97cc41be06b1c06fd2d67fa9e
beerkaya <berk-6023@yandex.com>
2022-01-21 20:41:12 +0300

    router entegration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store/store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store/store";
+// import "./assets/css/style.sass";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes: routes,
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+      };
+    }
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+});
 
 new Vue({
   store,
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
