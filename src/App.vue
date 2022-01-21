9237441c4803fbbf4332983f832999ab6c79b2d6
beerkaya <berk-6023@yandex.com>
2022-01-21 20:39:16 +0300

    Login and Register added.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 text-center"><h1>KAYABASE1</h1></div>
-    </div>
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
     name: "App",
-    created() {
-      console.log(this.$store.state.count)
-    }
   };
 </script>
 
 <style lang="scss">
-  @import "assets/css/style";
+  @import "./assets/css/style.sass";
 </style>
